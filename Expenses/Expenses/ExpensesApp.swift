b681018eaf9bfef68414207139c95eaf3c84a5f6
PaulErny <paul.erny@epitech.eu>
2024-05-01 17:15:02 +0200

    INIT - project creation

---
+++ Expenses/Expenses/ExpensesApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpensesApp.swift
+//  Expenses
+//
+//  Created by Paul Erny on 01/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct ExpensesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
